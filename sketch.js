f052fd287a946f3bd9ae958b075bce86d3121fb7
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2022-05-05 16:21:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,128 +1,45 @@
-let bloodCells;
-let State = 1;
-let BloodVessel;
-let BloodCell=1;
-let blur = 10;
-let audio = 1;
-let audioBG = 1;
-
-function preload() {
-  blood1 = loadImage("Images/B1.png");
-  blood2 = loadImage("Images/B2.png");
-  blood3 = loadImage("Images/B3.png");
-  BV = loadImage("Images/bloodvessel.png");
-  WBC = loadImage("Images/WBC.png")
-  heartbeat = loadSound('Sounds/heartbeat.mp3');
-  bgSong = loadSound("Sounds/BGsong.mp3");
+body {
+  margin: auto;
+  width: 640px; 
+  padding: 0px;
+  font-family: 'Avenir', sans-serif; 
+  color: "maroon";
+  overflow: hidden;
 }
-function setup() {
-  createCanvas(displayWidth, displayHeight);
-  BloodVessel = createSprite(windowWidth/2, windowHeight/2, 100, 100);
-  BloodVessel.addImage(BV);
-  BloodVessel.scale = windowWidth*0.002
-  heartbeat.play()
-  drawingContext.filter = "blur("+blur+"px)";
 
-  //
+/* Centered Image Code */
+
+.container {
+  height: 500px;
+  position: relative;
+  border: 3px solid #dbe4ed; /* Border color is optional */ 
+}
+
+ .center {
+  margin: 1;
+  position: absolute;
+  top: 50%;
+  left: 70%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  max-height: 35%;
+  max-width: 35%;
+  width: auto;
+  height: auto;
 
 }
-function draw() {
-  background("rgb(107,2,2)");
-  backgroundStart();
-  
-  if (State === 1) {
-    
-    if(heartbeat.isPlaying()!=true){
-      heartbeat.play()
-    }
-    
-  }
-  if (State === 2) {
-    backgroundStart();
-    heartbeat.setVolume(audio)
-    drawingContext.filter = "blur("+blur+"px)";
-    if(frameCount%5==0){
-      blur=blur-0.4
-      audio=audio-0.05
-    }
-    if(audio <= 0){
-      heartbeat.stop()
-      //song.play
-      if(bgSong.isPlaying()!=true){
-      bgSong.play()
-      bgSong.setVolume(0.055)
-      }
-    }
-    
-    
-    if(blur<=0){
-      blur=0
-    }
-  }
-  drawSprites();
-  
-}
-function backgroundStart() {
-  if (frameCount % 20=== 0) {
-    BloodCell = createSprite(windowWidth+50, 200, 0.1, 0.1);
-
-    x = Math.round(random(1, 3));
-    y = Math.round(random(1, 6));
-
-    if (x == 1) {
-      push()
-      translate(width / 2, height / 2);
-      rotate(PI / 3.0);
-      BloodCell = createSprite(windowWidth+50, random(windowHeight*0.35, windowHeight*0.58), 20, 20);
-      BloodCell.addImage(blood1);
-      BloodCell.velocityX = random(-2,-5);
-      BloodCell.scale = 0.2;
-      pop()
-    }
-    if (x == 2) {
-      push()
-      translate(width / 2, height / 2);
-      rotate(PI / 3.0);
-      BloodCell = createSprite(windowWidth+50, random(windowHeight*0.35, windowHeight*0.58), 20, 20);
-      BloodCell.addImage(blood2);
-      BloodCell.velocityX = random(-2,-5);
-      BloodCell.scale = 0.2;
-      pop()
-    }
-    if (x == 3) {
-      push()
-      rotate(PI / 3.0);
-      BloodCell = createSprite(windowWidth+50, random(windowHeight*0.35, windowHeight*0.58), 20, 20);
-      BloodCell.addImage(blood3);
-      BloodCell.velocityX = random(-2,-5);
-      BloodCell.scale = 0.2;
-      pop()
-    }
-    /* (x == 4) {
-      push()
-      rotate(PI / 3.0);
-      BloodCell = createSprite(windowWidth+50, random(windowHeight*0.35, windowHeight*0.58), 20, 20);
-      BloodCell.addImage(blood3);
-      BloodCell.velocityX = random(-2,-5);
-      BloodCell.scale = 0.2;
-      pop()
-    }*/
-  
-    if (y == 1) {
-      push()
-      rotate(PI / 3.0);
-      BloodCell = createSprite(windowWidth+50, random(windowHeight*0.35, windowHeight*0.58), 20, 20);
-      BloodCell.addImage(WBC);
- 
-     BloodCell.velocityX = random(-0.9,-3.25);
-      BloodCell.scale = 0.2;
-      pop()
-    }
-    BloodCell.y = Math.round(random(windowHeight*0.35, windowHeight*0.58));
-    BloodCell.velocityX = -7;
-    BloodCell.lifetime = 200;
-    BloodCell.rotationSpeed = random(1,3);
-    BloodCell.scale = BloodCell.scale*random(0.95,1.10)
-    //BloodGroup.add(BloodCell)
-  }
+p{
+  position: absolute;
+  font-size: 30px;
+  color: white;
+  top: -4%;
+  left: 13%;
+  font-family: "Monaco", Arial, monospace;
+  font-weight: bold;
+  text-shadow: 2px 2px black;
+  border-radius: 15px 160px 160px 5px;
+  background: #73AD21;
+  padding: 20px;
+  width: 650px;
+  height: 150px;
 }
